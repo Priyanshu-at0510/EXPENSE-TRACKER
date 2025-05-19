af9cd209ff2617abc1e5b00ee1c3d2ab73bedb87
priyanshu singh <ishusingh4006@gmail.com>
2025-05-19 23:49:10 +0530

    adding the connectDB function on config folder

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,2 +1,15 @@
 const mongoose=require("mongoose");
+require("dotenv").config;
+exports.connectDB=()=>{
+    mongoose.connect(process.env.MONGODB_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+    .then(()=>{console.log("DB connected successful")})
+    .catch((error)=>{
+        console.log("DB Facing connection issues");
+        console.log(error);
+        process.exit(1);
 
+    })
+}

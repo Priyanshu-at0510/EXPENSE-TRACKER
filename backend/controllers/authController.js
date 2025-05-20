375c5922c15a42b22b410f4578b9cff66dc6f66e
priyanshu singh <ishusingh4006@gmail.com>
2025-05-20 15:45:09 +0530

    adding the logic of registered User

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,8 +7,46 @@ const generateToken=(id)=>{
 }
 //register the user || signup
 exports.registerUser=async(req,res)=>{
+  const {fullName,email,password,profileImageUrl}=req.body;
 
-}
+  //validation:all field is present or not
+  if(!fullName || !email || !password ){
+    return res.status(400).json({
+        success:false,
+        message:"All field is required"
+    });
+    //check if user is already exists
+    try{
+        const existingUser=await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({
+                success:false,
+                message:"This email address is already registered"
+            })
+        }
+        //create the user
+        const user= await User.create({
+            fullName,
+            email,
+            password,
+            profileImageUrl
+        });
+
+        res.status(200).json({
+            id:user._id,
+            user,
+            token:generateToken(user._id),
+        });
+
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"Some internal server error occur during registering the User",
+            error:error.message
+        })
+    }
+  }
+};
 
 //login the user
 exports.loginUser=async(req,res)=>{

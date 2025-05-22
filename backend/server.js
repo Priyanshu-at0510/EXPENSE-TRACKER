9d65d40e52dbee70bca27ef6deb571a0b26423be
priyanshu singh <ishusingh4006@gmail.com>
2025-05-22 17:11:30 +0530

    updating the server.js file

--- backend/server.js
+++ backend/server.js
@@ -13,16 +13,23 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.CLIENT_URL,
+  methods:["GET","POST","PUT","DELETE"],
+  allowedHeaders:["Content-Type","Authorization"],
   credentials: true
 }));
 app.use(express.json());
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 connectDB();
+
+//adding all the routes
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income",incomeRoutes);
 app.use("/api/v1/expense",expenseRoutes);
 app.use("/api/v1/dashboard",dashboardRoutes);
 
+//starting the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

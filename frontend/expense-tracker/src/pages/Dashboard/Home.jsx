57bef29d72f1c31ff4cdd62df0551a5ef6a66afa
priyanshu singh <ishusingh4006@gmail.com>
2025-05-25 00:49:45 +0530

    update the dashboard with sideMenu, Add the Dashboard layout file along with Navbar and sideMenu

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+import {useUserAuth} from '../../hooks/useUserAuth';
 const Home=()=>{
+  console.log("going to DashboardLayout");
+  useUserAuth();
   return(
-    <div>
-      Home Page
-    </div>
+    <DashboardLayout activeMenu="Dashboard">
+
+      <div className='my-5 mx-auto'></div>
+    </DashboardLayout>
   );
 }
 export default Home;

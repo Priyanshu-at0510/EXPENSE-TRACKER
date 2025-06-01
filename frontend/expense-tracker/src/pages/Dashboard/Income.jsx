df3321c246290fc68c8368b9fc514fee518657ee
priyanshu singh <ishusingh4006@gmail.com>
2025-06-01 18:55:01 +0530

    adding the expense section where i show all the expense in the form of Line Chart

--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -68,7 +68,7 @@ const Income=()=>{
     } catch (error) {
       console.error(
         "Error adding income:",
-        error.responsible?.data?.message || error.message
+        error.response?.data?.message || error.message
       );
     }
   };
@@ -89,7 +89,30 @@ const Income=()=>{
   };
    
   //handle Download Income Details
-  const handleDownloadIncomeDetails=async ()=>{};
+  const handleDownloadIncomeDetails=async ()=>{
+    try {
+      const response=await axiosInstance.get(
+        API_PATHS.INCOME.DOWNLOAD_INCOME,
+        {
+          responseType:"blob"
+
+        }
+      );
+      //create a url for a blob
+      const url=window.URL.createObjectURL(new Blob([response.data]));
+      const link=document.createElement("a");
+      link.href=url;
+      link.setAttribute("download","income _details.xlsx");
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+      window.URL.revokeObjectURL(url);
+
+    } catch (error) {
+      console.error("error while downloaing income details",error);
+      toast.error("Failed to download income details.please try again later");
+    }
+  };
 
   useEffect(()=>{
     fetchIncomeDetails();

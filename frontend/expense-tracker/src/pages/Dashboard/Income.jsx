e91d456534711d2f0b05ab919c1688db7cbbb33e
priyanshu singh <ishusingh4006@gmail.com>
2025-06-01 16:51:17 +0530

    complete the income section

--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -8,8 +8,10 @@ import AddIncomeForm from '../../components/Income/AddIncomeForm';
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import IncomeList from '../../components/Income/IncomeList';
-
+import DeleteAlert from '../../components/DeleteAlert';
+import { useUserAuth } from '../../hooks/useUserAuth';
 const Income=()=>{
+  useUserAuth();
   const [incomeData,setIncomeData]=useState([]);
   const [loading,setLoading]=useState(false);
   const [openDeleteAlert,setOpenDeleteAlert]=useState({
@@ -72,7 +74,19 @@ const Income=()=>{
   };
 
   //Delete Income
-  const deleteIncome=async (id)=>{};
+  const deleteIncome=async (id)=>{
+    try {
+      await axiosInstance.delete(API_PATHS.INCOME.DELETE_INCOME(id));
+      setOpenDeleteAlert({show:false,data:null});
+      toast.success("Income details deleted successfully");
+      fetchIncomeDetails();
+    } catch (error) {
+      console.error(
+        "Error while deleting Income",
+        error.response?.data?.message || error.message
+      );
+    }
+  };
    
   //handle Download Income Details
   const handleDownloadIncomeDetails=async ()=>{};
@@ -107,6 +121,16 @@ const Income=()=>{
         >
           <AddIncomeForm onAddIncome={handleAddIncome}/>
         </Modal>
+        <Modal
+         isOpen={openDeleteAlert.show}
+         onClose={()=>setOpenDeleteAlert({show:false,data:null})}
+         title="Delete Income"
+         >
+          <DeleteAlert
+            content="Are you sure you want to delete this income details?"
+            onDelete={()=>deleteIncome(openDeleteAlert.data)}
+          />
+         </Modal>
       </div>
     </DashboardLayout>
   );
